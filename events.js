5ec71310d2c69f2f8b1a6928ae0068e27ac997d8
Lua H. Moon M. C. <luahelenammc@gmail.com>
2026-03-01 13:24:21 -0300

    Update events.js

--- events.js
+++ events.js
@@ -1,61 +1,118 @@
-// events.js — só dados (arrays) 
-// Você pode editar aqui sem mexer no app.js.
+// events.js — banco de dados (arrays)
+// Edite aqui e recarregue o index.html
 
 window.COUNTDOWN_EVENTS = [
+  // FÉRIAS
   {
-    id: "ferias",
+    id: "inicio-ferias",
     title: "Início das férias",
     date: "2026-02-28",
     emoji: "🏖️",
     color: "#00f5d4"
   },
+
+  // TER, 03 MAR
   {
-    id: "dentista",
-    title: "Dentista",
+    id: "consulta-online-dra-rafaela",
+    title: "Consulta online — Dra Rafaela",
+    date: "2026-03-03",
+    emoji: "💻",
+    color: "#7df9ff"
+  },
+  {
+    id: "dentista-dra-andrea",
+    title: "Dentista — Dra Andrea",
     date: "2026-03-03",
     emoji: "🦷",
     color: "#6fb6ff"
   },
+
+  // QUA, 04 MAR
   {
-    id: "coletivo-trans",
-    title: "Coletivo trans",
+    id: "pes-na-gi",
+    title: "Fazer pés na Gi",
+    date: "2026-03-04",
+    emoji: "🦶",
+    color: "#b388ff"
+  },
+  {
+    id: "cerveja-com-isabelly",
+    title: "Cerveja com Isabelly",
+    date: "2026-03-04",
+    emoji: "🍻",
+    color: "#ff57ba"
+  },
+
+  // QUI, 05 MAR
+  {
+    id: "tela-protecao-avisar-mae",
+    title: "Tela de proteção — avisar mãe",
+    date: "2026-03-05",
+    emoji: "🛡️",
+    color: "#ffe66d"
+  },
+
+  // DOM, 08 MAR
+  {
+    id: "cantinho-trans",
+    title: "Participação no Cantinho Trans",
     date: "2026-03-08",
     emoji: "🏳️‍⚧️",
     color: "#ff57ba"
   },
+
+  // QUA, 11 MAR
   {
-    id: "depilacao",
+    id: "depilacao-laser-11",
     title: "Depilação a laser",
     date: "2026-03-11",
     emoji: "✨",
-    color: "#ffe66d"
+    color: "#00ff6a"
   },
+
+  // QUI, 12 MAR
   {
-    id: "esmaltação",
-    title: "Esmaltação (Dudinha)",
+    id: "depilacao-laser-12",
+    title: "Depilação a laser",
+    date: "2026-03-12",
+    emoji: "✨",
+    color: "#00ff6a"
+  },
+
+  // SEX, 13 MAR
+  {
+    id: "esmaltacao-em-gel-dudinha",
+    title: "Esmaltação em Gel — Dudinha",
     date: "2026-03-13",
     emoji: "💅",
     color: "#b388ff"
   },
+
+  // SÁB, 14 MAR
   {
-    id: "cilios",
-    title: "Cílios",
-    date: "2026-03-16",
+    id: "cilios-na-rafa-manutencao",
+    title: "Cílios na Rafa — Manutenção",
+    date: "2026-03-14",
     emoji: "👁️",
-    color: "#00ff6a"
-  }, 
-  {
-    id: "fim-ferias",
-    title: "Fim das férias",
-    date: "2026-03-17",
-    emoji: "🌅",
-    color: "#ff7ad9"
+    color: "#00f5d4"
   },
+
+  // SEG, 16 MAR
+
   {
     id: "viagem-sp",
     title: "Viagem para SP",
-    date: "2026-03-18",
+    date: "2026-03-16",
     emoji: "🧳",
     color: "#7df9ff"
+  },
+
+  // 18/03
+  {
+    id: "trabalho",
+    title: "Volta ao trabalho",
+    date: "2026-03-18",
+    emoji: "💼",
+    color: "#ff7ad9"
   }
 ];
